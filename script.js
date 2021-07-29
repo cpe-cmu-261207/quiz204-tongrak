56170a4bf3b0602f82f08c86a5b4d9d293a38a3b
Oliver Tongrak <tongrak_ru@cmu.ac.th>
2021-07-29 14:19:43 +0700

    AH

--- script.js
+++ script.js
@@ -5,13 +5,32 @@ const length = document.getElementById('length')
 const color = document.querySelector('#color')
 
 const autherCode = 630610730;
+const lengthVal = length.value;
+const wholePara =  document.getElementById('text');
+let clickOnce = false;
 
-const cout = console.log
+const searchBut = document.getElementById('search');
 
-// define more constants and variables here
+const autherBox = document.getElementById("author");
+
+const cout = console.log;
+
+searchBut.onclick = () => {
+
+}
+
 
 btn_toggle.onclick = () => {
-  
+  // cout(author)
+  if(clickOnce){
+    autherBox.innerHTML = "630610730 TONGRAK RUENTO";
+    btn_toggle.innerHTML = "Display Calculator"
+    clickOnce = false;
+  }else{
+    autherBox.innerHTML = autherCode-lengthVal;
+    btn_toggle.innerHTML = "Display Auther"
+    clickOnce = true;
+  }
 }
 
 
