8c481ca5bd2644af0faa482b2cad419d6dc54353
Oliver Tongrak <tongrak_ru@cmu.ac.th>
2021-07-29 13:58:50 +0700

    Innit

--- script.js
+++ script.js
@@ -4,10 +4,16 @@ const btn_toggle = document.querySelector("#toggle");
 const length = document.getElementById('length')
 const color = document.querySelector('#color')
 
+const autherCode = 630610730;
+
+const cout = console.log
+
 // define more constants and variables here
 
 btn_toggle.onclick = () => {
-  // your code here
+  
 }
+
+
 
 // more codes for Search and Reset buttons here
